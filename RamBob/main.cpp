2ec923ecf68c585f5fe06d9d02dbbee3c2f86fa6
Christian Treffs <christian.treffs@7p-group.com>
2016-09-11 12:56:01 +0200

    update Xcode 8 project

--- RamBob/main.cpp
+++ RamBob/main.cpp
@@ -18,70 +18,70 @@
 using namespace std;
 
 int main(int argc, const char * argv[]) {
-    const char *usageHelp = "usage: RamBob [disk size ...] [B|KB|MB|GB] [disk name ...]";
-    
-    const double BLOCK_SIZE = 512;
-    
-    // units that can be used
-    const char *units[4]{"B","KB","MB","GB"};
-    double      UNITS[4]{1.0,1000.0,1000000.0,1000000000.0};
-    
-    // default parameter values
-    string diskName = "UNNAMED";
-    double diskSize = -1;
-    int unit = -1;
-    
-    // print help if not the right arg number
-    if(argc != 4) {
-        cout << usageHelp << endl;
-        return 1;
-    }
-    
-    // right arg number
-    if(argc == 4) {
-        // determine disk size
-        try {
-            diskSize   = stod(argv[1]);
-        } catch (...)
-        {
-            cout << "no valid disk size" << endl;
-            cout << usageHelp << endl;
-            return 2;
-        }
-        
-        
-        // determine unit
-        string unitStr = argv[2];
-        for(int i=0; i < 4; ++i) {
-            if(string(units[i]) == unitStr) {
-                unit = UNITS[i];
-                break;
-            }
-        }
-        if(unit == -1) {
-            cout << "no valid unit" << endl;
-            cout << usageHelp << endl;
-            return 3;
-        }
-        
-        
-        // determine disk name
-        string optName = string(argv[3]);
-        if(!optName.empty()) {
-            diskName = optName;
-        }
-    }
-    
-    
-    
-    // calculate disk size
-    double diskSizeByte = diskSize  * unit;
-    const unsigned long diskSizeBlocks = static_cast<unsigned long>(floor(diskSizeByte/BLOCK_SIZE));
-    
-    // creation string
-    ostringstream createRamDisk;
-    createRamDisk << "diskutil erasevolume HFS+ \"" << diskName << "\" `hdiutil attach -owners on -nomount ram://" << diskSizeBlocks << "`\n";
-    
-    // send to system
-    return system(createRamDisk.str().c_str());
+	const char *usageHelp = "usage: RamBob [disk size ...] [B|KB|MB|GB] [disk name ...]";
+	
+	const double BLOCK_SIZE = 512;
+	
+	// units that can be used
+	const char *units[4]{"B","KB","MB","GB"};
+	double      UNITS[4]{1.0,1000.0,1000000.0,1000000000.0};
+	
+	// default parameter values
+	string diskName = "UNNAMED";
+	double diskSize = -1;
+	int unit = -1;
+	
+	// print help if not the right arg number
+	if(argc != 4) {
+		cout << usageHelp << endl;
+		return 1;
+	}
+	
+	// right arg number
+	if(argc == 4) {
+		// determine disk size
+		try {
+			diskSize   = stod(argv[1]);
+		} catch (...)
+		{
+			cout << "no valid disk size" << endl;
+			cout << usageHelp << endl;
+			return 2;
+		}
+		
+		
+		// determine unit
+		string unitStr = argv[2];
+		for(int i=0; i < 4; ++i) {
+			if(string(units[i]) == unitStr) {
+				unit = UNITS[i];
+				break;
+			}
+		}
+		if(unit == -1) {
+			cout << "no valid unit" << endl;
+			cout << usageHelp << endl;
+			return 3;
+		}
+		
+		
+		// determine disk name
+		string optName = string(argv[3]);
+		if(!optName.empty()) {
+			diskName = optName;
+		}
+	}
+	
+	
+	
+	// calculate disk size
+	double diskSizeByte = diskSize  * unit;
+	const unsigned long diskSizeBlocks = static_cast<unsigned long>(floor(diskSizeByte/BLOCK_SIZE));
+	
+	// creation string
+	ostringstream createRamDisk;
+	createRamDisk << "diskutil erasevolume HFS+ \"" << diskName << "\" `hdiutil attach -owners on -nomount ram://" << diskSizeBlocks << "`\n";
+	
+	// send to system
+	return system(createRamDisk.str().c_str());
 }
